b2d4ebfc0c26ce15831d0560486578552c1a0a2a
jswelsh <james.sinclair.welsh@gmail.com>
2019-08-20 18:11:37 +0000

    fix formating

--- game.rb
+++ game.rb
@@ -3,7 +3,7 @@ require_relative "question"
 require_relative "player"
 
 class Game
-  attr_accessor :p1, :p2
+  attr_accessor :p1, :p2, :current_round 
   MAX_ROUNDS = 20
 
   def initialize(p1, p2)
@@ -15,25 +15,29 @@ class Game
   end
 
   def next_round
+    puts " ------------round #{@current_round}--------------"
     question = Question.new
     puts "#{@current_player.name} your turn."
     question.new_question
     question.validate_answer(gets.chomp.to_i) || @current_player.lose_life
  
     @current_player = @current_player == self.p1 && self.p2 || self.p1
+    @current_round -= 1
   end
 end
-
+puts "input users name"
 game = Game.new(gets.chomp, gets.chomp)
 
-  while game.p1.check_if_alive && game.p2.check_if_alive do
+  while game.current_round > 0 && game.p1.check_if_alive && game.p2.check_if_alive do
     game.next_round
     game.next_round
     game.p1.current_life
     game.p2.current_life
   end
 
-  if game.p1.check_if_alive
+  if game.p1.check_if_alive && game.p2.check_if_alive
+    puts "Tie"
+  elsif game.p1.check_if_alive
     puts "#{game.p1.name} wins"
   else
     puts "#{game.p2.name} wins"
